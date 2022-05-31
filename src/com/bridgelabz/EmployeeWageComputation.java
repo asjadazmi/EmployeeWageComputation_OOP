8a7e243dd621ebe8c299daf68895c0df3defc3c1
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 13:56:31 +0530

    Added:Welcome to the Employee Wage Computation

---
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class EmployeeWageComputation {
+	public static void main(String args[]) {
+		System.out.println("welcome to the computation wage problem....");
+	}
+}
