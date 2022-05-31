2585c307c1d4123cec0626073ceee12e3df530aa
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 14:20:22 +0530

    Added:Attendence of the employee

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
+import java.util.*;
 
 public class EmployeeWageComputation {
+	public static void check() {
+		int IS_PRESENT=1;
+		double check =Math.floor(Math.random()*10)%2;
+		if (check==IS_PRESENT) {
+			System.out.println("Employee is present....");
+		}
+		else {
+			System.out.println("Employee is absent.....");
+		}
+}
 	public static void main(String args[]) {
-		System.out.println("welcome to the computation wage problem....");
+		check();
 	}
 }
