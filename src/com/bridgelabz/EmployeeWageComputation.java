3106fc058c2ad6bb086f2dfaf06950c701672381
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 14:44:11 +0530

    Added:Added the wage of part time

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,13 +3,18 @@ import java.util.*;
 
 public class EmployeeWageComputation {
     static final int IS_PRESENT=1;
+    static final int IS_PART_TIME=2;
 	static final int Wage_per_HR=20;
+	static final int PART_TIME_hrs=4;
 	static final int Full_day_HRS=8;
 	public static void check() {
 		 int tototalwage=0;
-		double check =Math.floor(Math.random()*10)%2;
+		double check =Math.floor(Math.random()*10)%3;
 		if (check==IS_PRESENT) {
 			tototalwage=Wage_per_HR*Full_day_HRS;
+		}
+		else if(check==IS_PART_TIME) {
+			tototalwage=PART_TIME_hrs*Wage_per_HR;
 		}
 		else {
 			tototalwage=0;
