d66f3005eaf8a5df95b76ba3ae0a58a8d0151735
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 15:15:08 +0530

    Added:Wages for the 20 days

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -7,23 +7,27 @@ public class EmployeeWageComputation {
 	static final int Wage_per_HR=20;
 	static final int PART_TIME_hrs=4;
 	static final int Full_day_HRS=8;
+	public static final int NUM_OF_WORKING_DAYS=20;
 	public static void check() {
-		 int tototalwage=0;
-		int check =(int)Math.floor(Math.random()*10)%3;
-		switch (check) 
-		{
-		case IS_PRESENT:
-			tototalwage=Wage_per_HR*Full_day_HRS;
-			break;
-		
-		case IS_PART_TIME:
-			tototalwage=PART_TIME_hrs*Wage_per_HR;
-		    break;
-		default:
-			tototalwage=0;
-		}
-		System.out.println("tototalwage: "+(tototalwage));
-}
+		int emphrs=0,empwage=0,totalEmpwage=0;
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++) {
+			int empcheck=(int)Math.floor(Math.random()*10)%3;
+			switch (empcheck) {
+			   case IS_PRESENT:
+				   emphrs=8;
+				   break;
+			   case IS_PART_TIME:
+				   emphrs=4;
+				   break;
+			   default:
+				   emphrs=0;
+				   
+			}
+			empwage=emphrs*Wage_per_HR;
+			totalEmpwage+= empwage;
+			System.out.println(" emp wage="+empwage+"emphrs "+" "+emphrs);
+			System.out.println("total emp wage"+totalEmpwage+"$");
+		}}
 	public static void main(String args[]) {
 		check();
 		
