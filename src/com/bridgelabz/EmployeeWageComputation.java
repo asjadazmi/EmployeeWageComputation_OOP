f9398a36e8b7b32f10704bb3ee5509076157b498
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-31 15:01:58 +0530

    Added:Using the Switch Case

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -9,14 +9,17 @@ public class EmployeeWageComputation {
 	static final int Full_day_HRS=8;
 	public static void check() {
 		 int tototalwage=0;
-		double check =Math.floor(Math.random()*10)%3;
-		if (check==IS_PRESENT) {
+		int check =(int)Math.floor(Math.random()*10)%3;
+		switch (check) 
+		{
+		case IS_PRESENT:
 			tototalwage=Wage_per_HR*Full_day_HRS;
-		}
-		else if(check==IS_PART_TIME) {
+			break;
+		
+		case IS_PART_TIME:
 			tototalwage=PART_TIME_hrs*Wage_per_HR;
-		}
-		else {
+		    break;
+		default:
 			tototalwage=0;
 		}
 		System.out.println("tototalwage: "+(tototalwage));
