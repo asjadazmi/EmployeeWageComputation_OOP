eb72f7e5fcc6d6a5fa4005619b71a7c830759ad8
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-06-02 13:23:36 +0530

    Added: Total employee wage for each company

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,8 +4,22 @@ import java.util.*;
 public class EmployeeWageComputation {
     static final int IS_PRESENT=1;
     static final int IS_PART_TIME=2;
-	
-	public static int computeEmpWage(String Company,int Wage_per_HR,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH) {
+    private final String Company;
+    private static int Wage_per_HR;
+    private int totalEmpWage;
+    private static int NUM_OF_WORKING_DAYS ;
+    private static int MAX_HRS_IN_MONTH;
+    
+    public  EmployeeWageComputation(String Company,int Wage_per_HR,int NUM_OF_WORKING_DAYS,int MAX_HRS_IN_MONTH) {
+    	this.Company=Company;
+    	this.Wage_per_HR=Wage_per_HR;
+//		this.totalEmpWage = totalEmpWage2;
+    	this.NUM_OF_WORKING_DAYS=NUM_OF_WORKING_DAYS;
+    	this.MAX_HRS_IN_MONTH=MAX_HRS_IN_MONTH;
+		
+	 }
+    
+	public static void computeEmpWage() {
 		int totalworkingdays=0;
 		int emphrs=0,totalemphrs=0;
 		while(totalemphrs<=MAX_HRS_IN_MONTH&&totalworkingdays<NUM_OF_WORKING_DAYS) {
@@ -26,13 +40,21 @@ public class EmployeeWageComputation {
 			System.out.println("day: "+totalworkingdays+++" emphrs "+emphrs);
 			
 		}
+	
 		int totalempwage=totalemphrs*Wage_per_HR;
 		System.out.println("total emp wage"+totalempwage);
-		return totalempwage;
+		
 		}
+	@Override
+	public String toString() {
+		return "total employee wage for company:"+Company+"is: "+totalEmpWage;
+		
+	}
 	public static void main(String args[]) {
-		computeEmpWage("reliance",50,2,10);
-		computeEmpWage("Walmart",30,4,14);
+		EmployeeWageComputation dmart=new EmployeeWageComputation("dmart",20,2,10);
+		dmart.computeEmpWage();
+		
+		
 		
 	}
 }
